2b6a54a30d9e77d2579e2164213bd0f12a02af56
Jacobsky <jacobsky@users.noreply.github.com>
2025-10-13 22:52:36 +0900

    Finished blog handler
    Blog endpoint completed, added an extra blog entry.
    
    Signed-off-by: Jacobsky <jacobsky@users.noreply.github.com>

--- internal/components/blog/blog.go
+++ internal/components/blog/blog.go
@@ -2,6 +2,7 @@ package blog
 
 import (
 	"apparently-typing/static"
+	"log/slog"
 	"net/http"
 	"path"
 	"strconv"
@@ -29,9 +30,14 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		post_id := r.PathValue("id")
 		if post_id == "" {
 			list(w, r)
+			return
 		}
-
-		showpost(post_id, w, r)
+		index, err := strconv.Atoi(r.PathValue("id"))
+		if err != nil {
+			http.Error(w, "Malformed post ID", http.StatusBadRequest)
+			return
+		}
+		showpost(index, w, r)
 		// Check
 
 	default:
@@ -52,19 +58,15 @@ func list(w http.ResponseWriter, r *http.Request) {
 	templ.Handler(List("Blog Posts", titles)).ServeHTTP(w, r)
 }
 
-func showpost(id string, w http.ResponseWriter, r *http.Request) {
-	index, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {
-		http.Error(w, "Malformed post ID", http.StatusBadRequest)
-		return
-	}
+func showpost(index int, w http.ResponseWriter, r *http.Request) {
 	posts, err := static.BlogFiles.ReadDir("blog")
 
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		slog.Error("Read Blog Files Error: ", "error", err)
 		return
 	}
-	if index > len(posts) || index < 0 {
+	if index >= len(posts) || index < 0 {
 		http.Error(w, "Post not found", http.StatusNotFound)
 		return
 	}
@@ -72,6 +74,7 @@ func showpost(id string, w http.ResponseWriter, r *http.Request) {
 	filecontent, err := static.BlogFiles.ReadFile(path.Join("blog", filename))
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		slog.Error("Read Files Content Error: ", "error", err)
 		return
 	}
 
@@ -79,16 +82,20 @@ func showpost(id string, w http.ResponseWriter, r *http.Request) {
 	filedate, filetitle, found := strings.Cut(datetitle, "-")
 	if !found {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		slog.Error("Parsing filename error", "error", err)
 		return
 	}
-	date, err := time.Parse("YYYYMMDD", filedate)
-	if err != nil {
+	year, err := strconv.Atoi(filedate[0:4])
+	month, err2 := strconv.Atoi(filedate[5:6])
+	day, err3 := strconv.Atoi(filedate[7:8])
+	if err != nil || err2 != nil || err3 != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		slog.Error("Time converstaion error", "error", err, "error", err2, "error", err3)
 		return
 	}
 
 	var post = BlogPost{
-		Date:    date,
+		Date:    time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC),
 		Title:   filetitle,
 		Content: string(filecontent),
 	}
